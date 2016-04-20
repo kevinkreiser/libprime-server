aa0a6b5d28ec5f891a70dadc0f6c73d0e9b6f528
Kevin Kreiser <kevinkreiser@gmail.com>
2016-04-20 08:14:29 -0400

    thanks to @sneetsher for showing me how to get the lib packages building!

--- build.sh
+++ build.sh
@@ -10,19 +10,11 @@ pushd build
 #sudo apt-get install autoconf automake libtool make gcc-4.9 g++-4.9 lcov
 sudo apt-get install libcurl4-openssl-dev libzmq3-dev
 git clone --branch 0.3.2 --recursive  https://github.com/kevinkreiser/prime_server.git
-pushd prime_server
-./autogen.sh
-popd
 tar pczf prime_server.tar.gz prime_server
-pushd prime_server
-./configure
-make -j
-sudo make install
-popd
 rm -rf prime_server
 
 #start building the package
-sudo apt-get install dh-make bzr-builddeb
+sudo apt-get install dh-make dh-autoreconf bzr-builddeb
 bzr dh-make libprime-server 0.3.2 prime_server.tar.gz
 rm -rf libprime-server/debian
 cp -rp ../debian libprime-server
