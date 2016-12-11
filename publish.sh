a5557e17522527bc42c2e3d8d4a1880926d76ef1
Kevin Kreiser <kevinkreiser@gmail.com>
2016-12-11 10:14:27 -0500

    fix paths to lauchpad

--- publish.sh
+++ publish.sh
@@ -11,13 +11,13 @@ cd ${DISTRIBUTIONS[0]}/unpinned
 bzr init
 bzr add
 bzr commit -m "Packaging for ${VERSION}-0ubuntu1."
-bzr push --overwrite bzr+ssh://kevinkreiser@bazaar.launchpad.net/~kevinkreiser/+junk/valhalla_${VERSION}-0ubuntu1
+bzr push --overwrite bzr+ssh://kevinkreiser@bazaar.launchpad.net/~kevinkreiser/+junk/prime-server_${VERSION}-0ubuntu1
 cd -
 
 #sign and push each package to launchpad
 for dist in ${DISTRIBUTIONS[@]}; do
 	for pin in pinned unpinned; do
 		debsign ${dist}/${pin}/*source.changes
-		dput ppa:valhalla-routing/valhalla ${dist}/${pin}/*source.changes
+		dput ppa:kevinkreiser/prime-server ${dist}/${pin}/*source.changes
 	done
 done
